fe28560dbb917a550c982524776cffdd2bb5a01d
karuta <meta7p.takapon@gmail.com>
2018-04-15 20:39:11 +0900

    録画・停止ボタンを表示させる

--- src/renderer/Capture.js
+++ src/renderer/Capture.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import styles from '../css/app.css';
-
+import IconButton from 'material-ui/IconButton';
+import FiberManualRecord from 'material-ui-icons/FiberManualRecord';
+import Stop from 'material-ui-icons/Stop';
 
 export default class Capture extends Component {
 
+  showSvg(isRecord) {
+    if (isRecord) {
+       return <FiberManualRecord id={styles.capture__action_svg_record} />
+    }
+    return <Stop id={styles.capture__action_svg_stop} />
+  }
+
   render() {
-    const {stream, onClick} = this.props;
+    const {stream, isRecord, onClick} = this.props;
     const url = stream && URL.createObjectURL(stream) || '';
     return (
       <div id={styles.capture}>
-        <video id={styles.capture__video} src={url} autoPlay onClick={onClick}></video>
+        <video id={styles.capture__video} src={url} autoPlay></video>
+        <IconButton id={styles.capture__action} onClick={onClick}>
+          {this.showSvg(isRecord)}
+        </IconButton>
       </div>
     );
   }
