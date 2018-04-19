8dd06a28a4796afe20e78c3b9a37b139bbf014fb
karuta0825 <magune3g@gmail.com>
2018-04-19 23:26:21 +0900

    サムネカードにかえる

--- src/renderer/Thumbnails.js
+++ src/renderer/Thumbnails.js
@@ -3,6 +3,7 @@ import styles from '../css/app.css';
 import List from 'material-ui/List';
 import Typography from 'material-ui/Typography';
 import { ListItem, ListItemText } from 'material-ui/List';
+import Paper from 'material-ui/Paper';
 
 export default class Thumbnails extends Component {
 
@@ -17,22 +18,46 @@ export default class Thumbnails extends Component {
     const {imgs, selectThumbnail} = this.props
     return (
       <div id={styles.thumbnails}>
-        <List component="nave">
-        {
-          imgs.map((item,idx) => {
-            return (
-              <ListItem className={styles.thumbnails__item} button onClick={() => { selectThumbnail(item)}}>
-                <img className={styles.thumbnails__item__img} src={item.thumbnail.toDataURL()} />
-                <div className={styles.thumbnails__item__name}>
-                  <Typography variant='body2'>{this.setCharlength(item.name)}</Typography>
-                </div>
-              </ListItem>
-            )
-          })
-        }
-        </List>
+        <ul style={{padding:0}}>
+          {
+            imgs.map((item,idx) => {
+              return (
+                <Paper className={styles.thumbnails__item} elevation={1} onClick={() => { selectThumbnail(item)}}>
+                  <img className={styles.thumbnails__item__img} src={item.thumbnail.toDataURL()} />
+                  <div className={styles.thumbnails__item__name}>
+                    <Typography variant='body2'>{this.setCharlength(item.name)}</Typography>
+                  </div>                
+                </Paper>
+              );
+            })
+          }
+        </ul>
       </div>
     );
   }
+
+  // render() {
+  //   const {imgs, selectThumbnail} = this.props
+  //   return (
+  //     <div id={styles.thumbnails}>
+  //       <List component="nave">
+  //       {
+  //         imgs.map((item,idx) => {
+  //           return (
+  //             <ListItem className={styles.thumbnails__item} button onClick={() => { selectThumbnail(item)}}>
+  //               <div className={styles.thumbnails__item__imgBox}>
+  //                 <img className={styles.thumbnails__item__img} src={item.thumbnail.toDataURL()} />
+  //               </div>
+  //               <div className={styles.thumbnails__item__name}>
+  //                 <Typography variant='body2'>{this.setCharlength(item.name)}</Typography>
+  //               </div>
+  //             </ListItem>
+  //           )
+  //         })
+  //       }
+  //       </List>
+  //     </div>
+  //   );
+  // }
 
 }
