62e7570a74f31844dad3fcf95aeb8023d78d3869
karuta <meta7p.takapon@gmail.com>
2018-05-02 14:29:32 +0900

    仕様変更: 更新日付に変更する

--- src/renderer/components/Player/PlayList.jsx
+++ src/renderer/components/Player/PlayList.jsx
@@ -27,7 +27,7 @@ export default class PlayList extends Component {
             files.map((v,k) => (
               <div className={ (k === selectedItemNum) ? styles.item_selected : styles.item} onClick={() => selectVideo(k)}>
                 <Typography variant='body2'>
-                  <div className={styles.item__date}>{moment(v.birthday).format('YYYY年MM月DD日')}</div>
+                  <div className={styles.item__date}>{moment(v.mtime).format('YYYY年MM月DD日')}</div>
                   <div className={styles.item__name}>{v.name}</div>
                   <div className={styles.item__size}>{Math.floor( v.size/1000000 * Math.pow(10, 1) ) / Math.pow(10, 1) + 'MB'}</div>
                 </Typography>
